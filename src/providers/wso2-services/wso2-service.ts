803f646877405bf64b695a91e66e7b4e9295e918
Benjamin <benjamin.daubry@student.uclouvain.be>
2018-04-12 21:55:01 +0200

    useless

--- src/providers/wso2-services/wso2-service.ts
+++ src/providers/wso2-services/wso2-service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
-import { wso2Header } from '../../app/variables-config';
+//import { wso2Header } from '../../app/variables-config';
 import { wso2HeaderStudent } from '../../app/variables-config';
 import 'rxjs/add/operator/catch';
 import {Observable} from 'rxjs/Rx';
