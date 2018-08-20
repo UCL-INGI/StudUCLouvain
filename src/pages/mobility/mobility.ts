ad2776c3a2a0cbb979df09e99cf8878a071f8437
BenJ-MACOS <benjamin.Daubry@student.uclouvain.be>
2018-08-19 23:05:37 -0700

    mobi

--- src/pages/mobility/mobility.ts
+++ src/pages/mobility/mobility.ts
@@ -87,7 +87,7 @@ export class MobilityPage {
     }
 
     //Check if the app is installed, if yes launch app else return the user to the market
-    this.appAvailability.check(app).then(
+    this.appAvailability.check(check).then(
       () => { // success callback
         const browser = this.iab.create(page.appUrl, '_system');
         browser.close();
