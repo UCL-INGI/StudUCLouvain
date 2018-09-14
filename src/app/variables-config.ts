8b75967d0e920166d617196cf492a642c1d41c69
BenJneB <benjamin.daubry@student.uclouvain.be>
2018-09-14 08:57:07 +0200

    Update variables-config.ts

--- src/app/variables-config.ts
+++ src/app/variables-config.ts
@@ -1,3 +1,2 @@
 export var jsApiKey:string = "YOUR_API_KEY";
-export var wso2Header:string = "AUTHORIZATION_HEADER_FOR_WSO2_CALLS";
 export var wso2HeaderStudent:string = "AUTHORIZATION_HEADER_FOR_WSO2_LOGIN";
