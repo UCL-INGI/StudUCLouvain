602815893bf7999fd9863838190e7e3756f4e1b9
BenJneB <benjamin.daubry@student.uclouvain.be>
2018-09-26 09:55:43 +0200

    Emergency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,21 @@
     Copyright (c)  Université catholique Louvain.  All rights reserved
     Authors :  Jérôme Lemaire and Corentin Lamy
     Date : July 2017
-    This file is part of UCLCampus
+    This file is part of Stud.UCLouvain
     Licensed under the GPL 3.0 license. See LICENSE file in the project root for full license information.
 
-    UCLCampus is free software: you can redistribute it and/or modify
+    Stud.UCLouvain is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    UCLCampus is distributed in the hope that it will be useful,
+    Stud.UCLouvain is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with UCLCampus.  If not, see <http://www.gnu.org/licenses/>.
+    along with Stud.UCLouvain.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 import { Component, ViewChild } from '@angular/core';
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -277,9 +277,12 @@ export class HomePage {
     this.translateService.get('GUINDAILLE.HELP7').subscribe((res:string) => {msg7=res;});
     this.translateService.get('GUINDAILLE.HELP8').subscribe((res:string) => {msg8=res;});
     this.translateService.get('GUINDAILLE.HELP9').subscribe((res:string) => {msg9=res;});
+    let out:string;
+    this.translateService.get('GUINDAILLE.HELP18').subscribe((res:string) => {out=res;});
     let alert = this.alertCtrl.create({
       title: urg,
-      message: "<p> <strong>" + msg1 + "</strong>: <h1><a href=\"tel:112\">112</a></h1> </p> <p><strong>" + msg2 + "</strong>: <h1><a href=\"tel:010 47 22 22\">010 47 22 22</a></h1> </p> <p> <br>" + msg3 + " <br><br> <strong>" + msg4 + "</strong> " + msg5 + "<br> <strong>" + msg6 + "</strong> " + msg7 + "<br> <strong>" + msg8 + "</strong> " + msg9 +"<br>",
+      message: "<p> <strong>" + msg1 + "</strong>: <br><font size=\"+1\"><a href=\"tel:010 47 22 22\">010 47 22 22</a></font> </p> <p><strong>" + msg2 + "</strong>: <br><font size=\"+1\"><a href=\"tel:010 47 24 24\">010 47 24 24</a></font> <br>ou<br> <font size=\"+1\"><a href=\"tel:02 764 93 93\">02 764 93 93</a></font> <br>(Woluwe - St Gilles - Tournai)<br> ou <br><font size=\"+1\"><a href=\"tel:065 32 35 55\">065 32 35 55</a></font> (Mons)</p> <p><strong>Contact:</strong> <a href=\"mailto:security@uclouvain.be\">security@uclouvain.be</a></p> <p><strong>" + out + ":</strong> <font size=\"+1\"><a href=\"tel:112\">112</a></font></p>  <p> <br>" + msg3 + " <br><br> <strong>" + msg4 + "</strong> " + msg5 + "<br> <strong>" + msg6 + "</strong> " + msg7 + "<br> <strong>" + msg8 + "</strong> " + msg9 +"<br>",
+      cssClass: "emergency",
       buttons: [
       {
         text:close,
