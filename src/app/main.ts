da441dc8bdbc5d8c77156bf8822d35f4e62f4f0c
BenJ <benjamin.daubry@uclouvain.be>
2020-01-26 05:36:51 +0100

    Update main.ts

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,10 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
- if (environment.production) {
-  enableProdMode();
-  if(window){
-    window.console.log=function(){};
-  }
+enableProdMode();
+if(window){
+  window.console.log=function(){};
 }
 platformBrowserDynamic().bootstrapModule(AppModule);
