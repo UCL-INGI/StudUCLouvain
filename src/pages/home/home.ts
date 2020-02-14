64f5e3de89be926ee3959a87fce590bba7858e03
BenJ <benjamin.daubry@uclouvain.be>
2020-02-14 03:00:59 +0100

    Again

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -315,27 +315,7 @@ export class HomePage {
     const alert = this.alertCtrl.create({
       title: urg,
       message:
-        '<p> <strong>' +
-        msg1 +
-        '</strong>: <br><font size="+1"><a href="tel:010 47 22 22">010 47 22 22</a></font> </p> <p><strong>' +
-        msg2 +
-        '</strong>: <br><font size="+1"><a href="tel:010 47 24 24">010 47 24 24</a></font> <br>ou<br> <font size="+1"><a href="tel:02 764 93 93">02 764 93 93</a></font> <br>(Woluwe - St Gilles - Tournai)<br> ou <br><font size="+1"><a href="tel:065 32 35 55">065 32 35 55</a></font> (Mons)</p> <p><strong>Contact:</strong> <a href="mailto:security@uclouvain.be">security@uclouvain.be</a></p> <p><strong>' +
-        out +
-        ':</strong> <font size="+1"><a href="tel:112">112</a></font></p>  <p> <br>' +
-        msg3 +
-        ' <br><br> <strong>' +
-        msg4 +
-        '</strong> ' +
-        msg5 +
-        '<br> <strong>' +
-        msg6 +
-        '</strong> ' +
-        msg7 +
-        '<br> <strong>' +
-        msg8 +
-        '</strong> ' +
-        msg9 +
-        '<br>',
+        this.getAlertEmergencyMsg(msg1, msg2, out, msg3, msg4, msg5, msg6, msg7, msg8, msg9),
       cssClass: 'emergency',
       buttons: [
         {
@@ -345,5 +325,31 @@ export class HomePage {
       ]
     });
     alert.present();
+  }
+
+  private getAlertEmergencyMsg(
+    msg1: any, msg2: any, out: string, msg3: any, msg4: any, msg5: any, msg6: any, msg7: any, msg8: any, msg9: string
+  ): string {
+    return '<p> <strong>' +
+      msg1 +
+      '</strong>: <br><font size="+1"><a href="tel:010 47 22 22">010 47 22 22</a></font> </p> <p><strong>' +
+      msg2 +
+      '</strong>: <br><font size="+1"><a href="tel:010 47 24 24">010 47 24 24</a></font> <br>ou<br> <font size="+1"><a href="tel:02 764 93 93">02 764 93 93</a></font> <br>(Woluwe - St Gilles - Tournai)<br> ou <br><font size="+1"><a href="tel:065 32 35 55">065 32 35 55</a></font> (Mons)</p> <p><strong>Contact:</strong> <a href="mailto:security@uclouvain.be">security@uclouvain.be</a></p> <p><strong>' +
+      out +
+      ':</strong> <font size="+1"><a href="tel:112">112</a></font></p>  <p> <br>' +
+      msg3 +
+      ' <br><br> <strong>' +
+      msg4 +
+      '</strong> ' +
+      msg5 +
+      '<br> <strong>' +
+      msg6 +
+      '</strong> ' +
+      msg7 +
+      '<br> <strong>' +
+      msg8 +
+      '</strong> ' +
+      msg9 +
+      '<br>';
   }
 }
