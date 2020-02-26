e2f9c8ff8e59efc4d354e541ce65cc9d8f75df31
BenJ <benjamin.daubry@uclouvain.be>
2020-02-26 01:49:26 +0100

    remove useless

--- test-config/mocks-ionic.ts
+++ test-config/mocks-ionic.ts
@@ -16,71 +16,16 @@ export abstract class BaseMock {
 }
 
 export class PlatformMock {
-  public ready(): Promise<string> {
-    return new Promise(resolve => {
-      resolve('READY');
-    });
-  }
-
-  public getQueryParam() {
-    return true;
-  }
-
-  public registerBackButtonAction(fn: Function, priority?: number): Function {
-    return () => true;
-  }
-
-  public hasFocus(ele: HTMLElement): boolean {
-    return true;
-  }
-
-  public doc(): HTMLDocument {
-    return document;
-  }
-
   public is(): boolean {
     return true;
-  }
-
-  public getElementComputedStyle(container: any): any {
-    return {
-      paddingLeft: '10',
-      paddingTop: '10',
-      paddingRight: '10',
-      paddingBottom: '10'
-    };
-  }
-
-  public onResize(callback: any) {
-    return callback;
-  }
-
-  public registerListener(
-    ele: any,
-    eventName: string,
-    callback: any
-  ): Function {
-    return () => true;
   }
 
   public win(): Window {
     return window;
   }
 
-  public raf(callback: any): number {
-    return 1;
-  }
-
   public timeout(callback: any, timer: number): any {
     return setTimeout(callback, timer);
-  }
-
-  public cancelTimeout(id: any) {
-    // do nothing
-  }
-
-  public getActiveElement(): any {
-    return document['activeElement'];
   }
 }
 
@@ -96,53 +41,12 @@ export class SplashScreenMock extends SplashScreen {
   }
 }
 
-export class NavMock {
-  public pop(): any {
-    return new Promise(function (resolve: Function): void {
-      resolve();
-    });
-  }
-
-  public push(): any {
-    return new Promise(function (resolve: Function): void {
-      resolve();
-    });
-  }
-
-  public getActive(): any {
-    return {
-      instance: {
-        model: 'something'
-      }
-    };
-  }
-
-  public setRoot(): any {
-    return true;
-  }
-
-  public registerChildNav(nav: any): void {
-    return;
-  }
-}
-
 export class NavParamsMock {
   data = {};
 
   get(param) {
     return this.data[param];
   }
-}
-
-export class TranslateServiceMock {
-  data = {};
-
-  public get(key: any): any {
-    return this.data[key];
-  }
-}
-
-export class DeepLinkerMock {
 }
 
 export class MenuMock {
@@ -163,36 +67,6 @@ export class MenuMock {
   }
 }
 
-export class MenuControllerMock {
-  public static instance(menu?: MenuMock): any {
-    const m = menu || MenuMock.instance();
-    const instance = jasmine.createSpyObj('MenuController', [
-      'close',
-      'enable',
-      'get',
-      'getMenus',
-      'getOpen',
-      'isEnabled',
-      'isOpen',
-      'open',
-      'swipeEnable',
-      'toggle'
-    ]);
-    instance.close.and.returnValue(Promise.resolve());
-    instance.enable.and.returnValue(m);
-    instance.get.and.returnValue(m);
-    instance.getMenus.and.returnValue([m]);
-    instance.getOpen.and.returnValue(m);
-    instance.isEnabled.and.returnValue(true);
-    instance.isOpen.and.returnValue(false);
-    instance.open.and.returnValue(Promise.resolve());
-    instance.swipeEnable.and.returnValue(m);
-    instance.toggle.and.returnValue(Promise.resolve());
-
-    return instance;
-  }
-}
-
 const METHODS2 = ['present', 'dismiss', 'onDidDismiss'];
 
 export class AlertMock extends BaseMock {
@@ -208,17 +82,6 @@ export class AlertMock extends BaseMock {
 }
 
 const METHODS = ['create'];
-
-export class AlertControllerMock extends BaseMock {
-  constructor(alertMock?: AlertMock) {
-    super('AlertController', METHODS);
-    this.spyObj.create.and.returnValue(alertMock || new AlertMock());
-  }
-
-  public static instance(alertMock?: AlertMock): any {
-    return new AlertControllerMock(alertMock);
-  }
-}
 
 const METHODS3 = ['present', 'dismiss', 'setContent', 'setSpinner'];
 
@@ -245,11 +108,6 @@ export class LoadingControllerMock {
 export class UserServiceMock {
   constructor() {
   }
-  storage = {
-    get() {
-      return new Promise(() => {});
-    }
-  };
   getCampus() {
     return 'LLN';
   }
@@ -266,19 +124,13 @@ export class Wso2ServiceMock {
   }
 }
 
-export class RssServiceMock {
-  constructor() {
-  }
-}
-
 export class MockCacheStorageService extends CacheStorageService {
     constructor(a, b) {
         super(a, b);
     }
 
     public ready() {
-        let promise: Promise<LocalForage>;
-        return new Promise<LocalForage>((resolve, reject) => {
+        return new Promise<LocalForage>((resolve) => {
             resolve();
         });
     }
@@ -286,27 +138,9 @@ export class MockCacheStorageService extends CacheStorageService {
 
 
 export class MockCacheService {
-    _storage: MockCacheStorageService;
 
     constructor(_storage) {
     }
-
-    getItem() {
-        return new Promise<any>((resolve, reject) => {
-            resolve();
-        });
-    }
-
-    removeItem() {
-    }
-
-    saveItem() {
-    }
-}
-
-export function newMockCacheService() {
-    let _storage: MockCacheStorageService;
-    return new MockCacheService(_storage);
 }
 
 export class ModalControllerMock extends ModalController {
@@ -385,12 +219,5 @@ export class MockAlertController {
         const toRet = new MockAlert(props);
         this.created.push(toRet);
         return Promise.resolve(toRet);
-    }
-
-    getLast() {
-        if (!this.created.length) {
-            return null;
-        }
-        return this.created[this.created.length - 1];
     }
 }
