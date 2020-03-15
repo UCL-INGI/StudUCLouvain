745871fed034b4ddb3f8ad8b113e7c98b1f6a46e
BenJ <benjamin.daubry@uclouvain.be>
2020-03-15 04:08:15 +0100

    src config files

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 
 enableProdMode();
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.log(err));
